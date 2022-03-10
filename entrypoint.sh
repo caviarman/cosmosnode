60d353a7f5a54634dd7608b46b6306fce777f937
Vladimir Ilin <v.ilin@evrone.com>
2022-03-10 10:07:50 +0300

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env sh
- 
-export PATH=$PATH:/go/bin
- 
-gaiad init stakematic --chain-id cosmoshub-4
- 
-(echo $KEYPASSWD; echo $KEYPASSWD) | gaiad keys add myval
- 
-echo $KEYPASSWD | gaiad add-genesis-account myval 100000000000stake
- 
-# Create a gentx.
-echo $KEYPASSWD | gaiad gentx myval 100000000stake --chain-id cosmoshub-4
- 
-# Add the gentx to the genesis file.
-gaiad collect-gentxs
+initNode() {
+    export PATH=$PATH:/go/bin
+    
+    gaiad init stakematic --chain-id cosmoshub-4
+    
+    (echo $KEYPASSWD; echo $KEYPASSWD) | gaiad keys add myval
+    
+    echo $KEYPASSWD | gaiad add-genesis-account myval 100000000000stake
+    
+    # Create a gentx.
+    echo $KEYPASSWD | gaiad gentx myval 100000000stake --chain-id cosmoshub-4
+    
+    # Add the gentx to the genesis file.
+    gaiad collect-gentxs
+}
+
+initNode
  
 gaiad start
